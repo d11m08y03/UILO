9474e76e5c25bcf5809238498b324b2e1417bdd3
d11m08y03 <zakariyyakurmally@gmail.com>
2025-01-22 21:43:59 +0400

    Add company draw page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,22 @@ import AccessDenied from "./pages/AccessDenied";
 import UploadCompanies from "./pages/UploadCompanies";
 import { LinksEnum } from "./lib/link";
 import Home from "./pages/Home";
+import CompanyDraw from "./pages/CompanyDraw";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={LinksEnum.HOME} element={<Home />} />
-        <Route path={LinksEnum.ACCESS_DENIED} element={<AccessDenied />} />
-        <Route
-          path={LinksEnum.UPLOAD_COMPANIES}
-          element={<UploadCompanies />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path={LinksEnum.HOME} element={<Home />} />
+				<Route path={LinksEnum.ACCESS_DENIED} element={<AccessDenied />} />
+				<Route path={LinksEnum.COMPANY_DRAW} element={<CompanyDraw />} />
+				<Route
+					path={LinksEnum.UPLOAD_COMPANIES}
+					element={<UploadCompanies />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- frontend/src/lib/link.ts
+++ frontend/src/lib/link.ts
@@ -2,4 +2,5 @@ export enum LinksEnum {
   HOME = "/", // Listing of companies
   ACCESS_DENIED = "/access-denied",
   UPLOAD_COMPANIES = "/upload-companies",
+  COMPANY_DRAW = "/company-draw",
 }
