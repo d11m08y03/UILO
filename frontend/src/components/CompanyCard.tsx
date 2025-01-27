2b5d78b1ea435fe777c890fdbfa741ef6690f8c4
d11m08y03 <zakariyyakurmally@gmail.com>
2025-01-27 23:05:17 +0400

    Fuck you

--- frontend/src/components/CompanyCard.tsx
+++ frontend/src/components/CompanyCard.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import {
   Card,
-  CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 
 type CompanyCardProps = {
   name: string;
@@ -15,21 +12,12 @@ type CompanyCardProps = {
   children?: React.ReactNode; 
 };
 
-const CompanyCard: React.FC<CompanyCardProps> = ({ name, logo, field, children }) => {
+const CompanyCard: React.FC<CompanyCardProps> = ({ name }) => {
   return (
-    <Card className=""> 
+    <Card className="border-l-4 border-gray-800"> 
       <CardHeader>
         <CardTitle className="text-lg">{name}</CardTitle>
       </CardHeader>
-      <CardContent>
-        <img src={logo} alt={name} className="w-auto h-[20vh] mx-auto" /> 
-        <div>{children}</div> 
-        <div>
-          {field.map((item, index) => (
-            <Badge key={index}>{item}</Badge> 
-          ))}
-        </div>
-      </CardContent>
     </Card>
   );
 };
