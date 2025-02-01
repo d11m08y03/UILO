a65ece5f51df1b9e74ebf3eefbad86a8ee998dce
d11m08y03 <zakariyyakurmally@gmail.com>
2025-02-01 21:27:27 +0400

    Clean branch

--- frontend/src/lib/link.ts
+++
@@ -1,7 +0,0 @@
-export enum LinksEnum {
-  HOME = "/",
-  ACCESS_DENIED = "/access-denied",
-  UPLOAD_COMPANIES = "/upload-companies",
-  COMPANY_DRAW = "/company-draw",
-  DRAW_SCREEN = "/draw-screen"
-}
--- internal/models/stands.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Model struct {
-	ID            string
-	CompanyID     string
-	Name          string
-	StandCategory Category
-}
